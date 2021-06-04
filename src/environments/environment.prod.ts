0ebf923038b746bd9048ca7f337b9d945138361a
unknown <crissilva.cs77@gmail.com>
2021-06-04 04:09:44 -0400

    settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7qBoyvSkNSL7AD_0kRj2T4EDZqHUDD6A",
+    authDomain: "test-project-7b335.firebaseapp.com",
+    projectId: "test-project-7b335",
+    storageBucket: "test-project-7b335.appspot.com",
+    messagingSenderId: "81386415224",
+    appId: "1:81386415224:web:22187f13c7e94e57c7798d",
+    measurementId: "G-7CYPVC2BPM"
+  }
 };
